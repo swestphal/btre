e6917f07f1fca0568f9aed375847f1b0885a34d6
Simone Westphal <hello@swestphal.io>
2021-03-14 09:27:43 +0100

    refactor: Add templates and messages to account

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
+from django.contrib import messages
 # Create your views here.
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        messages.error(request, 'Testing error')
+        redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        messages.error(request, 'Testing error')
+        return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
--- btre/settings.py
+++ btre/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,3 +137,9 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
